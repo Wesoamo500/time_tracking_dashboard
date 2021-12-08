61214484a321a7b149f6a6894788c3433d4f72f6
Wesoamo500 <94782675+Wesoamo500@users.noreply.github.com>
2021-12-07 17:14:31 -0800

    Add files via upload

---
+++ time_tracking.js
@@ -0,0 +1,45 @@
+function daily(){
+		document.getElementById('work').innerHTML = "5hrs";
+		document.getElementById('pd').innerHTML = "Previous day-7hrs"
+		document.getElementById('exercise').innerHTML = "1hr";
+		document.getElementById('expd').innerHTML = "Previous day-1hr"
+		document.getElementById('social').innerHTML = "1hr";
+		document.getElementById('sopd').innerHTML = "Previous day-3hrs";
+		document.getElementById('play').innerHTML = "1hr";
+		document.getElementById('plpd').innerHTML = "Previous day-2hrs";
+		document.getElementById('study').innerHTML = "0hrs";
+		document.getElementById('stpd').innerHTML = "Previous day-1hr";
+		document.getElementById('self').innerHTML = "0hrs";
+		document.getElementById('sepd').innerHTML = "Previous day-1hr"
+}
+function weekly(){
+		document.getElementById('work').innerHTML = "32hrs";
+		document.getElementById('pd').innerHTML = "Previous week-36hrs"
+		document.getElementById('exercise').innerHTML = "4hrs";
+		document.getElementById('expd').innerHTML = "Previous week-5hrs"
+		document.getElementById('social').innerHTML = "5hrs";
+		document.getElementById('sopd').innerHTML = "Previous week-10hrs";
+		document.getElementById('play').innerHTML = "10hrs";
+		document.getElementById('plpd').innerHTML = "Previous week-8hrs";
+		document.getElementById('study').innerHTML = "4hrs";
+		document.getElementById('stpd').innerHTML = "Previous week-7hrs";
+		document.getElementById('self').innerHTML = "2hrs";
+		document.getElementById('sepd').innerHTML = "Previous week-2hrs"
+}
+
+
+function monthly(){
+	document.getElementById('work').innerHTML = "103hrs";
+	document.getElementById('pd').innerHTML = "Previous month-128hrs";
+	document.getElementById('exercise').innerHTML = "11hrs";
+	document.getElementById('expd').innerHTML = "Previous month-18hrs";
+	document.getElementById('social').innerHTML = "21hrs";
+	document.getElementById('sopd').innerHTML = "Previous month-23hrs";
+	document.getElementById('play').innerHTML = "23hrs";
+	document.getElementById('plpd').innerHTML = "Previous month-29hrs"
+	document.getElementById('study').innerHTML = "13hrs";
+	document.getElementById('stpd').innerHTML = "Previous month-19hrs";
+	document.getElementById('self').innerHTML = "7hrs";
+	document.getElementById('sepd').innerHTML = "Previous month-11hrs"
+
+}
